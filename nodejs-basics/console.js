b6ff4103ab520d7e9d727beb3efb702bc6ce4770
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-27 18:13:56 +0300

    console

---
+++ nodejs-basics/console.js
@@ -0,0 +1,13 @@
+//Terminal run with params
+//node .\console.js message=hello spec=false
+
+function consoleToJSON() {
+  const c = {};
+  for (let i = 2; i < process.argv.length; i++) {
+    const arg = process.argv[i].split("=");
+    c[arg[0]] = arg[1] ? arg[1] : true;
+  }
+  return c;
+}
+
+console.log(consoleToJSON());
