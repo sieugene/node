f3da7dc3a9aae71e643a619acedf91b3b6b5fbed
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-27 17:53:25 +0300

    web server, html

--- nodejs-basics/index.js
+++ nodejs-basics/index.js
@@ -1,14 +1,36 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  if (req.method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(`
+      <h1>Form</h1>
+      <form method="post" action="/">
+        <input name="title" type="text"/>
+        <button type="submit">send</button>
+      </form>
+      `);
+  } else if (req.method === "POST") {
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8",
+    });
+    const body = [];
+    req.on("data", (data) => {
+      body.push(Buffer.from(data));
+    });
 
-  res.write("<h1>Hello from node js</h1>");
-  res.end(`
-  <div style="background: red; width: 200px; height: 200px;">
-    <h1>Test</h1>
-  </div>
-  `);
+    req.on("end", () => {
+      const message = body.toString().split("=")[1];
+      res.end(`
+      <h1>
+      Ваше сообщение:
+        ${message}
+      </h1>
+    `);
+    });
+  }
 });
 
 server.listen(3000, () => {
