df1a3c02a4fcd11b34c9b456eb2f71647c74f2cb
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-27 15:04:06 +0300

    http module

--- nodejs-basics/index.js
+++ nodejs-basics/index.js
@@ -1,2 +1,16 @@
-const user = require("./user");
-console.log("test", user);
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  res.write("<h1>Hello from node js</h1>");
+  res.end(`
+  <div style="background: red; width: 200px; height: 200px;">
+    <h1>Test</h1>
+  </div>
+  `);
+});
+
+server.listen(3000, () => {
+  console.log("Server started");
+});
--- nodejs-basics/user.js
+++
@@ -1,5 +0,0 @@
-const user = {
-  name: "test",
-};
-
-module.exports = user;
