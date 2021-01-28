d67739220c2c4b38b2239557f3e6e13109068189
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-28 12:29:58 +0300

    head title, render with data

--- node-express/index.js
+++ node-express/index.js
@@ -16,11 +16,24 @@ app.set("views", "views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Главная страница",
+    isHome: true,
+  });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get("/courses", (req, res) => {
+  res.render("courses", {
+    title: "Курсы",
+    isCourses: true,
+  });
+});
+
+app.get("/add", (req, res) => {
+  res.render("add", {
+    title: "Добавить курс",
+    isAdd: true,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
