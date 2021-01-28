c09d4e9c0d05a1be86bc2d4837dc24291dd96195
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-28 16:23:04 +0300

    dynamic params, layout

--- node-express/routes/courses.js
+++ node-express/routes/courses.js
@@ -4,11 +4,19 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   const courses = await Course.getAll();
-
   res.render("courses", {
     title: "Курсы",
     isCourses: true,
     courses,
+  });
+});
+
+router.get("/:id", async (req, res) => {
+  const course = await Course.getById(req.params.id);
+  res.render("course", {
+    layout: "empty",
+    title: `Курс ${course.title}`,
+    course,
   });
 });
 
