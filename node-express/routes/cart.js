16266e69692f2f8821cf03d98786bf1421aea892
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-29 22:16:18 +0300

    populate courses in cart, output, total price

--- node-express/routes/cart.js
+++ node-express/routes/cart.js
@@ -2,6 +2,19 @@ const { Router } = require("express");
 const Course = require("../models/course");
 const router = Router();
 
+function mapCartItems(cart) {
+  return cart.items.map((c) => ({
+    ...c.courseId._doc,
+    count: c.count,
+  }));
+}
+
+function computedPrice(courses) {
+  return courses.reduce((total, course) => {
+    return (total += course.price * course.count);
+  }, 0);
+}
+
 router.post("/add", async (req, res) => {
   const course = await Course.findById(req.body.id);
   await req.user.addToCart(course);
@@ -9,10 +22,13 @@ router.post("/add", async (req, res) => {
 });
 
 router.get("/", async (req, res) => {
+  const user = await req.user.populate("cart.items.courseId").execPopulate();
+  const courses = mapCartItems(user.cart);
+
   res.render("cart", {
     title: "Корзина",
-    courses: req.user.cart.items,
-    price: 100,
+    courses: courses,
+    price: computedPrice(courses),
     isCart: true,
   });
 });
