40819578657d750aa536799ce96f7780e0dc6b40
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-30 22:16:23 +0300

    encryption password

--- node-express/routes/auth.js
+++ node-express/routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const router = Router();
+const bcrypt = require("bcryptjs");
 const User = require("./../models/user");
 
 router.get("/login", async (req, res) => {
@@ -13,7 +14,7 @@ router.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const candidate = await User.findOne({ email });
     if (candidate) {
-      const areSame = password === candidate.password;
+      const areSame = await bcrypt.compare(password, candidate.password);
       if (areSame) {
         req.session.user = candidate;
         req.session.isAuth = true;
@@ -43,10 +44,11 @@ router.post("/register", async (req, res) => {
     if (candidate) {
       res.redirect("/auth/login#register");
     } else {
+      const hashPassword = await bcrypt.hash(password, 10);
       const user = new User({
         email,
         name,
-        password,
+        password: hashPassword,
         cart: {
           items: [],
         },
