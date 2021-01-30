31fa722c29252c55d62bed12ff505882d7092b3a
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-30 22:01:22 +0300

    register user

--- node-express/routes/auth.js
+++ node-express/routes/auth.js
@@ -20,6 +20,29 @@ router.post("/login", async (req, res) => {
   });
 });
 
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, confirm, name } = req.body;
+    const candidate = await User.findOne({ email });
+    if (candidate) {
+      res.redirect("/auth/login#register");
+    } else {
+      const user = new User({
+        email,
+        name,
+        password,
+        cart: {
+          items: [],
+        },
+      });
+      await user.save();
+      res.redirect("/auth/login#login");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.get("/logout", async (req, res) => {
   req.session.destroy(() => {
     res.redirect("/auth/login#login");
