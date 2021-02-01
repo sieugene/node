1b4d290cd9057e2c52556e87985eda399c2f16ba
sieugene <evgenii.sidelnikov@unitbean.io>
2021-02-01 16:49:06 +0300

    reset password, extend user schema, reset page, email reset with token

--- node-express/routes/auth.js
+++ node-express/routes/auth.js
@@ -3,8 +3,10 @@ const bcrypt = require("bcryptjs");
 const User = require("./../models/user");
 const reqEmail = require("../emails/registration");
 const keys = require("../keys");
+const crypto = require("crypto");
 const router = Router();
 const sgMail = require("@sendgrid/mail");
+const resetEmail = require("../emails/reset");
 sgMail.setApiKey(keys.SENDGRID_API_KEY);
 
 router.get("/login", async (req, res) => {
@@ -79,6 +81,39 @@ router.get("/logout", async (req, res) => {
   req.session.destroy(() => {
     res.redirect("/auth/login#login");
   });
+});
+
+router.get("/reset", async (req, res) => {
+  res.render("auth/reset", {
+    title: "Забыли пароль?",
+    error: req.flash("error"),
+  });
+});
+
+router.post("/reset", (req, res) => {
+  try {
+    crypto.randomBytes(32, async (err, buffer) => {
+      if (err) {
+        req.flash("error", "Что-то пошло не так, повторите попытку позже");
+        return res.redirect("/auth/reset");
+      }
+      const token = buffer.toString("hex");
+      const candidate = await User.findOne({ email: req.body.email });
+      if (candidate) {
+        candidate.resetToken = token;
+        //1 hour
+        candidate.resetTokenExp = Date.now() + 60 * 60 * 1000;
+        await candidate.save();
+        await sgMail.send(resetEmail(candidate.email, token));
+        res.redirect("/auth/login");
+      } else {
+        req.flash("error", "Такого email нет");
+        res.redirect("/auth/reset");
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
