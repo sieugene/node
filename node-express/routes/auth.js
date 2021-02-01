b33ede61a99df96c1f092e597637f9ace427b627
sieugene <evgenii.sidelnikov@unitbean.io>
2021-02-01 17:10:17 +0300

    reset post route

--- node-express/routes/auth.js
+++ node-express/routes/auth.js
@@ -120,7 +120,6 @@ router.get("/password/:token", async (req, res) => {
   if (!req.params.token) {
     return res.redirect("/auth/login");
   }
-
   try {
     const user = await User.findOne({
       resetToken: req.params.token,
@@ -135,6 +134,28 @@ router.get("/password/:token", async (req, res) => {
         userId: user._id.toString(),
         token: req.params.token,
       });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/password", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      _id: req.body.userId,
+      resetToken: req.body.token,
+      resetTokenExp: { $gt: Date.now() },
+    });
+    if (user) {
+      user.password = await bcrypt.hash(req.body.password, 10);
+      user.resetToken = undefined;
+      user.resetTokenExp = undefined;
+      await user.save();
+      res.redirect("/auth/login");
+    } else {
+      req.flash("loginError", "Время жизни токена истекло");
+      res.redirect("/auth/login");
     }
   } catch (error) {
     console.log(error);
