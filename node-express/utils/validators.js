126db0ecc5745b9fd57e60a588e15dc27df3d45d
sieugene <evgenii.sidelnikov@unitbean.io>
2021-02-02 14:48:29 +0300

    modified data sanitization

--- node-express/utils/validators.js
+++ node-express/utils/validators.js
@@ -14,18 +14,23 @@ exports.registerValidators = [
       } catch (error) {
         console.log(error);
       }
-    }),
+    })
+    .normalizeEmail(),
   body("password", "Пароль должен быть минимум 6 символовов")
     .isLength({ min: 6, max: 56 })
-    .isAlphanumeric(),
-  body("confirm").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Пароли должны совпадать");
-    } else {
-      return true;
-    }
-  }),
+    .isAlphanumeric()
+    .trim(),
+  body("confirm")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Пароли должны совпадать");
+      } else {
+        return true;
+      }
+    })
+    .trim(),
   body("name")
     .isLength({ min: 3 })
-    .withMessage("Имя должно быть минимум 3 символа"),
+    .withMessage("Имя должно быть минимум 3 символа")
+    .trim(),
 ];
