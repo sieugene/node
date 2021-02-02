9094f32b7abc316daf4e786e0641850e52767817
sieugene <evgenii.sidelnikov@unitbean.io>
2021-02-02 14:43:25 +0300

    async validate

--- node-express/routes/auth.js
+++ node-express/routes/auth.js
@@ -51,35 +51,27 @@ router.post("/login", async (req, res) => {
 
 router.post("/register", registerValidators, async (req, res) => {
   try {
-    const { email, password, confirm, name } = req.body;
-    const candidate = await User.findOne({ email });
-
+    const { email, password, name } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       req.flash("registerError", errors.array()[0].msg);
       return res.status(422).redirect("/auth/login#register");
     }
-
-    if (candidate) {
-      req.flash("registerError", "Пользователь с таким email уже существует");
-      res.redirect("/auth/login#register");
-    } else {
-      const hashPassword = await bcrypt.hash(password, 10);
-      const user = new User({
-        email,
-        name,
-        password: hashPassword,
-        cart: {
-          items: [],
-        },
-      });
-      await user.save();
-      try {
-        await sgMail.send(reqEmail(email));
-        res.redirect("/auth/login#login");
-      } catch (error) {
-        console.log(error);
-      }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      name,
+      password: hashPassword,
+      cart: {
+        items: [],
+      },
+    });
+    await user.save();
+    try {
+      await sgMail.send(reqEmail(email));
+      res.redirect("/auth/login#login");
+    } catch (error) {
+      console.log(error);
     }
   } catch (error) {
     console.log(error);
--- node-express/utils/validators.js
+++ node-express/utils/validators.js
@@ -1,7 +1,20 @@
 const { body } = require("express-validator");
+const User = require("./../models/user");
 
 exports.registerValidators = [
-  body("email").isEmail().withMessage("Введите корректный email"),
+  body("email")
+    .isEmail()
+    .withMessage("Введите корректный email")
+    .custom(async (value, { req }) => {
+      try {
+        const candidate = await User.findOne({ email: value });
+        if (candidate) {
+          return Promise.reject("Такой email уже занят");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }),
   body("password", "Пароль должен быть минимум 6 символовов")
     .isLength({ min: 6, max: 56 })
     .isAlphanumeric(),
