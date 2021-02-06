8964827f6a34dd20355a9172e31220f96ff39b37
sieugene <evgenii.sidelnikov@unitbean.io>
2021-02-07 01:16:37 +0300

    end refactoring

--- node-graphql/graphql/resolver.js
+++ node-graphql/graphql/resolver.js
@@ -1,35 +1,5 @@
 const Todo = require("../models/todo");
-
-const users = [
-  { name: "Test", age: 30, email: "tester@Mail.ru" },
-  { name: "Test1", age: 32, email: "tester2@Mail.ru" },
-  { name: "Test3", age: 33, email: "tester3@Mail.ru" },
-];
 module.exports = {
-  test() {
-    return {
-      count: Math.trunc(Math.random() * 10),
-      users,
-    };
-  },
-  random({ min, max, count }) {
-    const arr = [];
-    for (let i = 0; i < count; i++) {
-      const random = Math.random() * (max - min) + min;
-      arr.push(random);
-    }
-    return arr;
-  },
-  addTestUser({ user }) {
-    const candidate = {
-      name: user.name,
-      email: user.email,
-      age: Math.ceil(Math.random() * 30),
-    };
-    users.push(candidate);
-    return candidate;
-  },
-
   async getTodos() {
     try {
       return await Todo.findAll();
--- node-graphql/graphql/schema.js
+++ node-graphql/graphql/schema.js
@@ -1,23 +1,6 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
-    type User {
-        name: String!
-        email: String!
-        age: Int!
-    }
-
-    type TestType {
-        count: Int!
-        users: [User!]!
-    }
-
-    input UserInput {
-        name: String!
-        email: String!
-    }
-
-
     type Todo {
         id: ID!
         title: String!
@@ -31,16 +14,12 @@ module.exports = buildSchema(`
     }
 
     type Mutation {
-        addTestUser(user: UserInput!): User!
         createTodo(todo: TodoInput!): Todo!
         completeTodo(id: ID!): Todo!
         deleteTodo(id: ID!): Boolean!
     }
 
     type Query {
-        test: TestType!
-        random(min:Int!,max:Int!,count:Int): [Float!]!
-
         getTodos: [Todo!]
     }
 `);
