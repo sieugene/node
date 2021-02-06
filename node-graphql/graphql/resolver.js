ac84ae05dcf7e9b2a9ab30888389259d96fbc74d
sieugene <evgenii.sidelnikov@unitbean.io>
2021-02-07 00:26:48 +0300

    resolver, schema, query, mutations

--- node-graphql/graphql/resolver.js
+++ node-graphql/graphql/resolver.js
@@ -1,5 +1,30 @@
+const users = [
+  { name: "Test", age: 30, email: "tester@Mail.ru" },
+  { name: "Test1", age: 32, email: "tester2@Mail.ru" },
+  { name: "Test3", age: 33, email: "tester3@Mail.ru" },
+];
 module.exports = {
   test() {
-    return "Hello graphql";
+    return {
+      count: Math.trunc(Math.random() * 10),
+      users,
+    };
+  },
+  random({ min, max, count }) {
+    const arr = [];
+    for (let i = 0; i < count; i++) {
+      const random = Math.random() * (max - min) + min;
+      arr.push(random);
+    }
+    return arr;
+  },
+  addTestUser({ user }) {
+    const candidate = {
+      name: user.name,
+      email: user.email,
+      age: Math.ceil(Math.random() * 30),
+    };
+    users.push(candidate);
+    return candidate;
   },
 };
--- node-graphql/graphql/schema.js
+++ node-graphql/graphql/schema.js
@@ -1,7 +1,28 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
+    type User {
+        name: String!
+        email: String!
+        age: Int!
+    }
+
+    type TestType {
+        count: Int!
+        users: [User!]!
+    }
+
     type Query {
-        test: String!
+        test: TestType!
+        random(min:Int!,max:Int!,count:Int): [Float!]!
+    }
+
+    input UserInput {
+        name: String!
+        email: String!
+    }
+
+    type Mutation {
+        addTestUser(user: UserInput!): User!
     }
 `);
